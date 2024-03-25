f264faccfe4a2e4c7b694a0e82be61fba194d8de
YahavGanon <yahavganon86@gmail.com>
2024-03-25 16:08:59 +0200

    Change in root

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,10 +2,8 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
-
 import { AppFooter } from './cmp/AppFooter.jsx'
 import { AppHeader } from './cmp/AppHeader.jsx'
-
 
 export function App() {
     return (
