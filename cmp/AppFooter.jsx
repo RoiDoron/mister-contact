7a74c90d525d890a34e9267b3ba73bac179ec015
Roi Doron <roidoron6@gmail.com>
2024-03-25 17:56:59 +0200

    Add footer counter

--- cmp/AppFooter.jsx
+++ cmp/AppFooter.jsx
@@ -1,4 +1,10 @@
+const { useSelector } = ReactRedux
 
-export function AppFooter(){
-    
+export function AppFooter() {
+    const count = useSelector(storeState => storeState.contactModule.contacts.length)
+   
+   return <footer>
+        <h3>contacts counter :{count}</h3>
+        <h4>Created by Yahav Gonen & Roi Doron</h4>
+    </footer>
 }
